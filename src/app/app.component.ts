2c13717414b263e09f219dbed42dc63a0811eb67
frame88 <fleuzzi97@gmail.com>
2022-09-07 13:12:42 +0200

    scaffolding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<div><h1 class="text-danger">ciao</h1></div>',
+  styles: []
 })
 export class AppComponent {
   title = 'applicazioniModulari';
