f863275aa28838069f8ad22b95dc7d6fa68a31d2
frame88 <fleuzzi97@gmail.com>
2022-09-08 16:42:03 +0200

    Router children e router-outlet secondario

--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -3,17 +3,30 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { SigninComponent } from './components/signin/signin.component';
+import { LostpassComponent } from './components/lostpass/lostpass.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    SigninComponent,
+    LostpassComponent,
+    RegistrationComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
     RouterModule.forChild([
-      { path: '', component: LoginComponent}
+      { path: '',
+        component: LoginComponent,
+        children: [
+          {path:'signin', component: SigninComponent},
+          {path:'lostpass', component: LostpassComponent},
+          {path:'registration', component: RegistrationComponent}
+        ]
+      }
     ])
   ]
 })
